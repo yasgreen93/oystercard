1df17989a78216501ea5408154246c8707dcad01
eripheebs <erikapheby@gmail.com>
2016-03-07 17:41:20 +0000

    Touch in and out methods added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,6 +4,7 @@ MAXIMUM_TOPUP = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(cash)
@@ -11,7 +12,23 @@ MAXIMUM_TOPUP = 90
     @balance += cash
   end
 
+  def deduct(cash)
+    @balance -= cash
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
   attr_reader :balance
+
+  def in_journey?
+    @in_journey
+  end
 
 private
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,11 +10,40 @@ describe Oystercard do
 
   describe 'top_up' do
     it '> should top up the amount specified' do
-      expect(subject.top_up(5)).to eq(5)
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
     end
 
     it '> should have a maximum limit of £90' do
       expect{subject.top_up(100)}.to raise_error "Maximum limit is 90 pounds"
+    end
+
+  end
+
+  describe 'deduct' do
+
+    it '> should deduct from the balance the amount specified' do
+      subject.top_up(5)
+      expect(subject.deduct(4)).to eq(1)
+    end
+
+  end
+
+  describe 'touch_in' do
+
+    it '> should set card to in use when touched in' do
+      subject.touch_in
+      expect(subject.in_journey?).to eq(true)
+    end
+
+  end
+
+  describe 'touch_out' do
+
+    it '> should set a card to out of use when touched out' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey?).to eq(false)
     end
 
   end
