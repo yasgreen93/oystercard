1fd0c6a7e221af799d9da09bf9080dc83c368fbc
Claudia Green <claudia@claudiag.me.uk>
2016-03-07 16:38:49 +0000

    Step 4 completed.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
+  describe '#balance'do
+    it { expect(subject).to respond_to(:balance) }
+    it "expect balance to be 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
