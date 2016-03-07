765db8105ee2578cfa14ab841616243f0a8a0db6
Harsheet <harsheetpatel@hotmail.com>
2016-03-07 16:52:22 +0000

    Challenge 5 complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,8 +4,14 @@ describe Oystercard do
 
   describe '#balance'do
     it { expect(subject).to respond_to(:balance) }
+    it { expect(subject).to respond_to(:top_up).with(1).argument }
+
     it "expect balance to be 0" do
       expect(subject.balance).to eq 0
+    end
+
+    it 'can top up balance' do
+      expect{subject.top_up 5}.to change{subject.balance}.by 5
     end
   end
 end
