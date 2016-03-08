16d1c5949237bc5c2f9259fc41f8c9e241dd3e3e
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-08 12:02:06 +0000

    add travis CI maybe

--- feature_test.rb
+++ feature_test.rb
@@ -2,8 +2,10 @@ require './lib/oystercard.rb'
 
 oystercard = Oystercard.new
 
-oystercard.top_up(5)
+oystercard.top_up(50)
 
-oystercard.touch_in
+oystercard.touch_in('pepper station')
+oystercard.touch_out('im hungry staton')
 
-oystercard.touch_in(station_name)
+oystercard.journeys
+oystercard.balance
