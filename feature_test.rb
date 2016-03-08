fcc89506afb0aa117bd3c7a0d6893a4b4a334645
eripheebs <erikapheby@gmail.com>
2016-03-08 14:59:39 +0000

    Feature test for the future (how to create multiple stations in one go)

--- feature_test.rb
+++ feature_test.rb
@@ -1,4 +1,5 @@
 require './lib/oystercard.rb'
+require './lib/station.rb'
 
 oystercard = Oystercard.new
 
@@ -10,4 +11,20 @@ oystercard.touch_out('im hungry staton')
 p oystercard.journeys
 p oystercard.balance
 
-aldgate = Station.new
+aldgate = Station.new :aldgate, 1
+
+oystercard.touch_in(aldgate)
+
+p oystercard
+
+list = {"oxford circus": 1,
+"liverpool street": 1,
+"hackney": 2}
+
+station_instances_hash = Hash.new
+
+list.each do |key, value|
+    station_instances_hash[key.to_sym] = Station.new(key.to_sym, value)
+end
+
+p station_instances_hash
