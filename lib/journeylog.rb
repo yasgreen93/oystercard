c87b5e6f421c7e682c744d576d343d8db7de46b1
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-10 18:53:56 +0000

    Added list_journeys method, and create_journey and log journey private methods to refactor and DRY code

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -3,10 +3,17 @@ class JourneyLog
 
   def initialize(journey_class)
     @journey_class = journey_class
+    @journeys = []
+    @current_journey = nil
+  end
+
+  def list_journeys
+    @journeys.dup
   end
 
   def start(entry_station)
-    @current_journey = @journey_class.new(entry_station)
+    create_journey(entry_station)
+    log_journey
   end
 
   def finish(exit_station)
@@ -14,6 +21,14 @@ class JourneyLog
   end
 
   private
+
+  def create_journey(entry_station)
+    @current_journey = @journey_class.new(entry_station)
+  end
+
+  def log_journey
+    @journeys << @current_journey
+  end
 
   def current_journey
   end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,8 +1,24 @@
 require 'journeylog'
 
 describe JourneyLog do
+  subject(:journeylog) {described_class.new(journey_class)}
+  let(:journey_class) {double :journey_class, new: Journey}
+  let(:entry_station) {double :station}
+  let(:exit_station) {double :station}
+  let(:journey) {double :journey}
 
   it {is_expected.to respond_to(:start).with(1).argument}
   it {is_expected.to respond_to(:finish).with(1).argument}
+
+  describe '#start' do
+    # it 'should record a journey' do
+    #   journeylog.start(entry_station)
+    #   expect(journeylog.list_journeys).to include journey
+    # end
+  end
+
+  describe '#finish' do
+
+  end
 
 end
