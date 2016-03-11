60d61e333d773f5c6ffbd3a89e11cbe58ceef1de
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-11 12:01:27 +0000

    Created private methods in JourneyLog. Tidied up Oystercard

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -2,6 +2,8 @@ require_relative 'journey'
 
 class JourneyLog
 
+  attr_reader :current_journey
+
   def initialize(journey_class)
     @journey_class = journey_class
     @current_journey = nil
@@ -9,12 +11,29 @@ class JourneyLog
 
   def start(station)
     @current_journey = @journey_class.new(station)
-
   end
 
   def finish(station)
+    current_journey
     @current_journey.end_journey(station)
+    log
+    reset
   end
 
+  def journeys
+    @journeys.dup
+  end
 
+  private
+    def current_journey
+      @current_journey ||= @journey_class.new(nil)
+    end
+
+    def log
+      @journeys << @current_journey
+    end
+
+    def reset
+      @current_journey = nil
+    end
 end
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,7 +3,7 @@ require_relative 'journeylog'
 
 class Oystercard
 
-  attr_reader :balance, :journeys
+  attr_reader :balance
   MIN_BALANCE = 1
   MAX_BALANCE = 90
   MAX_ERROR = "Top up exceeds card's maximum balance of £#{MAX_BALANCE}."
@@ -11,8 +11,7 @@ class Oystercard
 
   def initialize
     @balance = 0
-    @journeys = []
-    @journey_log = JourneyLog.new(Journey)
+    @journey_log = JourneyLog.new(Journey) #=> MORE INDEPENDENCY
   end
 
   def top_up(amount)
@@ -22,33 +21,36 @@ class Oystercard
 
   def touch_in(station)
     raise MIN_ERROR if @balance < MIN_BALANCE
-    deduct(@current_journey.fare) if @current_journey != nil
-    @current_journey = Journey.new(station)
+    deduct if @journey_log.current_journey != nil
+    @journey_log.start(station)
   end
 
   def touch_out(station)
-    @current_journey == nil ? didnt_touch_in(station) : did_touch_in(station)
-    @journeys << @current_journey
-    @current_journey = nil
+    @journey_log.finish(station)
+    deduct
+
+    # @current_journey == nil ? didnt_touch_in(station) : did_touch_in(station)
+    # @journeys << @current_journey
+    # @current_journey = nil
   end
 
 
 
   private
 
-  def deduct(amount)
-    @balance -= amount
+  def deduct
+    @balance -= @journey_log.current_journey.fare
   end
 
-  def didnt_touch_in(end_station)
-    @current_journey = Journey.new(nil)
-    @current_journey.end_journey(end_station)
-    deduct(@current_journey.fare)
-  end
-
-  def did_touch_in(end_station)
-    @current_journey.end_journey(end_station)
-    deduct(@current_journey.fare)
-  end
+  # def didnt_touch_in(end_station)
+  #   @current_journey = Journey.new(nil)
+  #   @current_journey.end_journey(end_station)
+  #   deduct(@current_journey.fare)
+  # end
+  #
+  # def did_touch_in(end_station)
+  #   @current_journey.end_journey(end_station)
+  #   deduct(@current_journey.fare)
+  # end
 
 end
